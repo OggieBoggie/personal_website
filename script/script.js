df1cf61f86a2cfd6d36454697eaf7118e12bdaf6
OggieBoggie <tommy.nguyen.vanc@gmail.com>
2022-11-10 10:48:11 -0800

    Created an array with items called courseList

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const courseList = [];
+
+courseList[0] = {
+    code: "ACIT 1620",
+    name: "Web Fundamental Technologies"
+};
+
+courseList[1] = {
+    code: "ACIT 1515",
+    name: " Scripting for IT"
+};
+
+courseList[2] = {
+    code: "ACIT 1630",
+    name: "Database Systems"
+};
+
+
